cc8a2dc9d814809b85ee3206547a1902d1475f9d
Mahesh Kasodhan <kasaudhan.mahesh021@gmail.com>
2021-01-09 11:44:44 +0530

    build

---
+++ build/precache-manifest.d4ed45bc9275379083e08ab6d5da6151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df1c81e7965861dea9f88a8250e141a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "727dec39c8047a1c2071",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727dec39c8047a1c2071",
+    "url": "/static/js/main.bbdd3b1e.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
